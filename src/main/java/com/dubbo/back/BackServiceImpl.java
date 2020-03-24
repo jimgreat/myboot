02ee9667a34b4f9cd699436ea8e81595142ca388
jinming <jimgreat@126.com>
2020-03-24 19:00:33 +0800

    metadata

--- src/main/java/com/dubbo/back/BackServiceImpl.java
+++ src/main/java/com/dubbo/back/BackServiceImpl.java
@@ -2,7 +2,7 @@ package com.dubbo.back;
 
 import org.apache.dubbo.config.annotation.Service;
 
-@Service(version = "1.0.0")
+@Service(version = "1.0.0",parameters = {"metadata","remote"})
 public class BackServiceImpl implements BackService {
     @Override
     public String back(String msg) {
