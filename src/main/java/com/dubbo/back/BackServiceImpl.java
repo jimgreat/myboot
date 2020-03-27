8167df911e7d66582240a42f8d7387a0c55e5aef
jinming <jinming@xiaomi.com>
2020-03-27 12:00:25 +0800

    add db

--- src/main/java/com/dubbo/back/BackServiceImpl.java
+++ src/main/java/com/dubbo/back/BackServiceImpl.java
@@ -1,24 +1,13 @@
 package com.dubbo.back;
 
-import com.business.entity.Game;
-import com.business.service.GameService;
 import org.apache.dubbo.config.annotation.Service;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
 
 @Service(version = "1.0.0")
 public class BackServiceImpl implements BackService {
 
-    @Autowired
-    GameService gameService;
-
     @Override
     public String back(String msg) {
-
-        List<Game> li = gameService.list();
-        int cnt = li.size();
-
-        return "I'm Back 2 ["+msg+"] with cnt:"+cnt;
+        return "I'm Back 2 ["+msg+"] with cnt:";
     }
 }
