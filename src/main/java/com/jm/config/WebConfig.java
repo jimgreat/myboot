309951f5b23fe80c8269c7690a934f194bf393e6
jinming <jinming@xiaomi.com>
2020-03-27 13:36:01 +0800

    cnt

--- src/main/java/com/jm/config/WebConfig.java
+++ src/main/java/com/jm/config/WebConfig.java
@@ -1,30 +1,52 @@
 package com.jm.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.spring.stat.BeanTypeAutoProxyCreator;
+import com.alibaba.druid.support.spring.stat.DruidStatInterceptor;
 import com.jm.interceptor.MyInterceptor;
 import org.springframework.aop.Advisor;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.JdkRegexpMethodPointcut;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class WebConfig {
 
     @Bean
-    public MyInterceptor myInterceptor() {
-        return new MyInterceptor();
-    }
-
-    @Bean
-    public JdkRegexpMethodPointcut myPointcut(){
-        JdkRegexpMethodPointcut myPointcut = new JdkRegexpMethodPointcut();
-        String patterns = "com.jm.*.service.*";
-        //可以set多个
-        myPointcut.setPatterns(patterns);
-        return myPointcut;
+    public ServletRegistrationBean druidServletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new StatViewServlet());
+        registration.addUrlMappings("/druid/*");
+        return registration;
     }
 
     @Bean
     public Advisor druidStatAdvisor() {
-        return new DefaultPointcutAdvisor(myPointcut(), myInterceptor());
+        return new DefaultPointcutAdvisor(druidStatPointcut(), druidStatInterceptor());
     }
+
+    @Bean
+    public JdkRegexpMethodPointcut druidStatPointcut() {
+        JdkRegexpMethodPointcut druidStatPointcut = new JdkRegexpMethodPointcut();
+        String patterns = "com.jm.*.service.*";
+        //可以set多个
+        druidStatPointcut.setPatterns(patterns);
+        return druidStatPointcut;
+    }
+
+    @Bean
+    public DruidStatInterceptor druidStatInterceptor() {
+        return new DruidStatInterceptor();
+    }
+
+    @Bean
+    public BeanTypeAutoProxyCreator beanTypeAutoProxyCreator() {
+        BeanTypeAutoProxyCreator beanTypeAutoProxyCreator = new BeanTypeAutoProxyCreator();
+        beanTypeAutoProxyCreator.setTargetBeanType(DruidDataSource.class);
+        beanTypeAutoProxyCreator.setInterceptorNames("druidStatInterceptor");
+        return beanTypeAutoProxyCreator;
+    }
+
 }
