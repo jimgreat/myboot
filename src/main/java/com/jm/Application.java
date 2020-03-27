d86e892a9737513ef61c0571813337afc33d04b9
jinming <jinming@xiaomi.com>
2020-03-27 13:06:16 +0800

    cnt

--- src/main/java/com/jm/Application.java
+++ src/main/java/com/jm/Application.java
@@ -1,9 +1,11 @@
 package com.jm;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
+@MapperScan("com.jm.business.mapper")
 public class Application  {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
